84fc2db45a7c19214da8e1f9bccf24e1f97df771
Asbin Ghimire <ghimireasbin12@gmail.com>
2026-04-18 20:23:21 +0545

    Advanced Discovery: Implement Global Search, Pagination, and Class-Based Views

--- news/urls.py
+++ news/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('article/<int:pk>/', views.article_detail, name='article_detail'),
-    path('category/<str:category_name>/', views.category_view, name='category_view'),
+    path('', views.HomeView.as_view(), name='index'),
+    path('article/<int:pk>/', views.ArticleDetailView.as_view(), name='article_detail'),
+    path('category/<str:category_name>/', views.CategoryListView.as_view(), name='category_view'),
+    path('search/', views.ArticleSearchView.as_view(), name='search_view'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,24 +1,65 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Article
 
-def index(request):
-    all_articles = Article.objects.filter(published_at__isnull=False).order_by('-published_at')
-    featured_article = all_articles.first()
-    latest_articles = all_articles[1:11]  # Next 10 articles
-    categories = Article.CATEGORY_CHOICES
-    return render(request, 'news/index.html', {
-        'featured_article': featured_article,
-        'latest_articles': latest_articles,
-        'categories': categories
-    })
+from django.views.generic import ListView, DetailView
+from django.db.models import Q
+from .models import Article
 
-def article_detail(request, pk):
-    article = get_object_or_404(Article, pk=pk)
-    return render(request, 'news/article_detail.html', {'article': article})
+class HomeView(ListView):
+    model = Article
+    template_name = 'news/index.html'
+    context_object_name = 'latest_articles'
+    paginate_by = 6
 
-def category_view(request, category_name):
-    articles = Article.objects.filter(category=category_name).order_by('-published_at')
-    return render(request, 'news/category.html', {
-        'articles': articles,
-        'category_name': category_name
-    })
+    def get_queryset(self):
+        return Article.objects.filter(published_at__isnull=False).order_by('-published_at')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        all_articles = self.get_queryset()
+        context['featured_article'] = all_articles.first()
+        # Ensure latest_articles in the context starts from the 2nd article
+        context['latest_articles'] = all_articles[1:]
+        context['categories'] = Article.CATEGORY_CHOICES
+        return context
+
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'news/article_detail.html'
+    context_object_name = 'article'
+
+class CategoryListView(ListView):
+    model = Article
+    template_name = 'news/category.html'
+    context_object_name = 'articles'
+    paginate_by = 6
+
+    def get_queryset(self):
+        self.category_name = self.kwargs['category_name']
+        return Article.objects.filter(category=self.category_name).order_by('-published_at')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category_name'] = self.category_name
+        return context
+
+class ArticleSearchView(ListView):
+    model = Article
+    template_name = 'news/category.html'  # Reusing category grid layout
+    context_object_name = 'articles'
+    paginate_by = 6
+
+    def get_queryset(self):
+        query = self.request.GET.get('q')
+        if query:
+            return Article.objects.filter(
+                Q(title__icontains=query) |
+                Q(description__icontains=query) |
+                Q(content__icontains=query)
+            ).order_by('-published_at')
+        return Article.objects.none()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category_name'] = f"Search Results for '{self.request.GET.get('q', '')}'"
+        return context
